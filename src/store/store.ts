e94a7c2c59c24a4f675e782d461eef6fc20585fb
Jing Jing Zhang <92764807+jjzhang329@users.noreply.github.com>
2024-03-11 04:20:12 -0500

    cleaning

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import { configureStore} from '@reduxjs/toolkit';
 import rootReducer from '../reducers/rootReducer';
 
 const store = configureStore({
